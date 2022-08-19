6be3a8bd78ea1d4dca7833d140a82ea09a76c464
yanjinliang <yanjinliang@yunlizhihui.com>
2022-08-19 18:19:02 +0800

    asd

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# tob-umi-script
+# umi project
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ yarn
+```
+
+Start the dev server,
+
+```bash
+$ yarn start
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import styles from './index.less'
+
+export default function IndexPage() {
+  const calc = (a: number, b: number) => {
+    return a - b
+  }
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  )
+}
