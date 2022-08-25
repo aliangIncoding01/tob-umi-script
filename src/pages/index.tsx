2df8ac5df41fd6d178e80edecb40b8eeb7ad5a34
yanjinliang <yanjinliang@yunlizhihui.com>
2022-08-25 15:12:44 +0800

    feat: umi init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './index.less'
 
 const IndexPage = () => {
-  const a = (a, b) => {
+  const a = (a: number, b: number) => {
     return a + b
   }
   return (
